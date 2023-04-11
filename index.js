97983a668e13d90000c5980a8eecb74b0f3ff454
Denis <denisdoroshenko99@gmail.com>
2023-04-11 15:53:22 +0300

    added bootstrap and designed hypothesis page

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
+const path = require('path');
 const { app, BrowserWindow } = require('electron');
+
+const isDev = process.env.NODE_ENV !== 'development';
+const isMac = process.platform === 'darwin';
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Polinomial Calculations',
-        width: 500,
-        height: 600
+        width: isDev ? 1000 : 500,
+        height: 700
     });
 
-    
+    // Open devtools
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+
+   mainWindow.loadFile(path.join(__dirname, './components/hypothesisCheck.html')); 
 }
+
+app.whenReady().then(() => {
+    createMainWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit()
+  }
+});
